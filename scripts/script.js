e1c0edc7ce8d03bec9fb3f15de90fcc435a1fe11
Carlos-Marte <carlosmartedev@hotmail.com>
2022-10-10 22:10:40 -0400

    Input range cambiado

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,11 @@ const inputRange = document.querySelector("#input-range");
 const inputBoton = document.querySelector("#input-boton");
 const pPassword = document.querySelector("#p-password");
 
-inputRange.addEventListener('click', () => {
+inputRange.addEventListener('input', () => {
     const longitudP = inputRange.nextElementSibling;
 
     longitudP.innerHTML = `Longitud: ${inputRange.value}`;
-});
+}, false);
 
 inputBoton.addEventListener('click', () => {
     let randomPassword = generarPassword(inputRange.value);
