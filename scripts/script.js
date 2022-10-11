c1b013d9641d03dc6e48fd591620e8711f2e1327
Carlos-Marte <carlosmartedev@hotmail.com>
2022-10-11 19:45:26 -0400

    Boton reiniciar y mejora del eventlistener

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,44 @@
 import { generarPassword } from "./passwordAleatoria.js";
 import { copiarClipboard } from "./copyClipboard.js";
 
+// Elementos
 const inputRange = document.querySelector("#input-range");
 const pPassword = document.querySelector("#p-password");
 const contenedor = document.querySelector(".contenedor");
 
-contenedor.addEventListener('click', (event) => {
+// Actualiza el input range
+const actualizarRange = () => {
+    const longitudOutput = inputRange.nextElementSibling;
 
+    longitudOutput.innerHTML = `Longitud: ${inputRange.value}`;
+}
+
+// Contenedor event 
+contenedor.addEventListener('click', (event) => {
     // Botón generar
-    if(event.target.className === "contenedor__boton") {
+    if (event.target.className === "contenedor__boton") {
         let randomPassword = generarPassword(inputRange.value);
-    
+
         pPassword.innerHTML = randomPassword;
     }
 
     // Botón clipboard
-    if(event.target.className === "password__icon") {
+    if (event.target.className === "password__icon") {
         let texto = pPassword.textContent;
 
-        if(texto !== "¡Presiona generar!") {
+        if (texto !== "¡Presiona generar!") {
             copiarClipboard(texto);
         }
-     }
+    }
+
+    // Botón reiniciar
+    if (event.target.classList[1] === "contenedor__boton--reiniciar") {
+        pPassword.textContent = "¡Presiona generar!";
+        inputRange.value = 5;
+        actualizarRange();
+    }
 });
 
-inputRange.addEventListener('input', () => {
-    const longitudOutput = inputRange.nextElementSibling;
+// Input range event 
+inputRange.addEventListener('input', actualizarRange, false);
 
-    longitudOutput.innerHTML = `Longitud: ${inputRange.value}`;
-}, false);
